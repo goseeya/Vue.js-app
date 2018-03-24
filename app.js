88d73995d7d52380a6a1b1673b78af50a94f3d4d
goseeya <149542@gmail.com>
2018-03-24 15:51:04 +0100

    refs

--- app.js
+++ app.js
@@ -59,3 +59,16 @@ new Vue({
 new Vue({
     el: '#vue-app-two',
 })
+
+new Vue({
+    el: '#vue-app',
+    data: {
+        output: 'Your fav food'
+    },
+    methods: {
+        readRefs: function(){
+            console.log(this.$refs.test.innerText);
+            this.output = this.$refs.input.value;
+        }
+    }
+});
